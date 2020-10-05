128b39bbfab68c467731058d4740fd3f56be852a
Malo BREGER <malo.breger@epitech.eu>
2020-10-05 20:01:37 +0200

    Base

---
+++ NWR/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float PlayerSpeed;
+    private float HorizontalMove = 0.0f;
+    private float VerticalMove = 0.0f;
+    private CharacterController CharacterC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxis("Horizontal") * PlayerSpeed;
+        VerticalMove = Input.GetAxis("Vertical")* PlayerSpeed;
+
+    }
+
+    private void FixedUpdate()
+    {
+        CharacterC.Move(new Vector3(HorizontalMove * Time.deltaTime, VerticalMove * Time.deltaTime, 0.0f));
+    }
+}
