37dafb66d60b28cd1e39b8707ecebbc5fc3d6a20
Malo BREGER <malo.breger@epitech.eu>
2020-10-07 16:14:22 +0200

    Base movement and 1st level layout

--- NWR/Assets/Scripts/PlayerMovement.cs
+++ NWR/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float PlayerSpeed;
     private float HorizontalMove = 0.0f;
     private float VerticalMove = 0.0f;
+
+    // If you use Character controller remove comment
     private CharacterController CharacterC;
+    
     // Start is called before the first frame update
     void Start()
     {
-        CharacterC = GetComponent<CharacterController>();
+        // If you use Character controller remove comment
+        // CharacterC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -20,10 +24,19 @@ public class PlayerMovement : MonoBehaviour
         HorizontalMove = Input.GetAxis("Horizontal") * PlayerSpeed;
         VerticalMove = Input.GetAxis("Vertical")* PlayerSpeed;
 
+        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 lookAt = mouseScreenPosition;
+
+        float AngleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
+
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
     }
 
     private void FixedUpdate()
     {
-        CharacterC.Move(new Vector3(HorizontalMove * Time.deltaTime, VerticalMove * Time.deltaTime, 0.0f));
+        transform.position += new Vector3(HorizontalMove * Time.deltaTime, VerticalMove * Time.deltaTime, 0.0f) * PlayerSpeed * Time.deltaTime;
+        // CharacterC.Move(new Vector3(HorizontalMove * Time.deltaTime, VerticalMove * Time.deltaTime, 0.0f));
     }
 }
---
+++ NWR/Assets/Scripts/TurnOnLights.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnLights : MonoBehaviour
+{
+    private bool touchingGene = false;
+    private bool lightActive;
+    public GameObject globalLight;
+    public GameObject pointLight;
+    public float startDuration = 15;
+    private float timeRemaining;
+    private bool lampTurnon;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lampTurnon = true;
+        lightActive = false;
+        timeRemaining = startDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E");
+            if (touchingGene && lightActive == false)
+            {
+                TurnOn();
+                lightActive = true;
+            }
+            else
+                Debug.Log("already turned on");
+        }
+        if (Input.GetKeyDown(KeyCode.F) && timeRemaining > 0.0f)
+        {
+            lampTurnon = !lampTurnon;
+            pointLight.SetActive(lampTurnon);
+        }
+        if (lampTurnon && timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining <= 0)
+            {
+                lampTurnon = false;
+                pointLight.SetActive(false);
+                audioSource.Play();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter");
+        if (other.tag == "Generator")
+        {
+            touchingGene = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("exit");
+        if (other.tag == "Generator")
+        {
+            touchingGene = false;
+        }
+    }
+
+    private void TurnOn()
+    {
+        globalLight.SetActive(true);
+    }
+}
